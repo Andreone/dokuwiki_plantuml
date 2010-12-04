210bd00eb985bfe5ffc40401bad8ebc9e124f414
Andreone <fatal: Not a git repository (or any of the parent directories): .git>
2010-12-04 16:25:05 +0100

    removed plantuml.jar file from project

--- conf/default.php
+++ conf/default.php
@@ -1,7 +1,7 @@
 <?php
 
 $conf['java']='';
-$conf['jar']='plantuml/plantuml.jar';
+$conf['jar']='plantuml.jar';
 $conf['remote_url']='http://plantuml.no-ip.org/plantuml/uml';
 $conf['toolbar_button_enabled']='1';
 $conf['toolbar_button_icon']='png/icon1.png';
