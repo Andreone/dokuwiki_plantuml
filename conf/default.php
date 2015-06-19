f333c1d8427ee668aa0ac668a0d52674349df13c
Nicolas Fauvet <n.fauvet@abc-arbitrage.com>
2015-06-19 16:01:08 +0200

    updated default remote url to http://www.plantuml.com/plantuml/

--- conf/default.php
+++ conf/default.php
@@ -3,6 +3,6 @@
 $conf['render_local']='0';
 $conf['java']='';
 $conf['jar']='plantuml.jar';
-$conf['remote_url']='http://plantuml.no-ip.org/plantuml/uml';
+$conf['remote_url']='http://www.plantuml.com/plantuml/';
 $conf['button_enabled']='1';
 $conf['button_icon']='res/icon1.png';
