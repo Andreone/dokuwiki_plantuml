6bcc97a48f9a11cb924ade1545ea1f44a1762631
Christian <christian.mchugh@untsystem.edu>
2015-08-13 09:58:42 -0500

    Correct encoding for remote operations

--- syntax.php
+++ syntax.php
@@ -221,16 +221,69 @@ class syntax_plugin_plantuml extends DokuWiki_Syntax_Plugin {
             // remove @startuml and @enduml, as they are not required by the webservice 
             $uml = str_replace("@startuml\n", '', $uml);
             $uml = str_replace("\n@enduml", '', $uml);
-            $uml = str_replace("\n", '/', $uml);
-            $uml = urlencode($uml);
-            // decode encoded slashes (or plantuml server won't understand)
-            $uml = str_replace('%2F', '/', $uml);
 
-            $url = "$base_url/startuml/$uml";
+            $uml = $this->encodep($uml);
+            $url = "$base_url/$uml";
         }
 
         $img = $http->get($url);
         return $img ? io_saveFile($out, $img) : false;
+    }
+
+    function encodep($text) {
+         $data = utf8_encode($text);
+         $compressed = gzdeflate($data, 9);
+         return $this->encode64($compressed);
+    }
+
+    function encode6bit($b) {
+         if ($b < 10) {
+              return chr(48 + $b);
+         }
+         $b -= 10;
+         if ($b < 26) {
+              return chr(65 + $b);
+         }
+         $b -= 26;
+         if ($b < 26) {
+              return chr(97 + $b);
+         }
+         $b -= 26;
+         if ($b == 0) {
+              return '-';
+         }
+         if ($b == 1) {
+              return '_';
+         }
+         return '?';
+    }
+
+    function append3bytes($b1, $b2, $b3) {
+         $c1 = $b1 >> 2;
+         $c2 = (($b1 & 0x3) << 4) | ($b2 >> 4);
+         $c3 = (($b2 & 0xF) << 2) | ($b3 >> 6);
+         $c4 = $b3 & 0x3F;
+         $r = "";
+         $r .= $this->encode6bit($c1 & 0x3F);
+         $r .= $this->encode6bit($c2 & 0x3F);
+         $r .= $this->encode6bit($c3 & 0x3F);
+         $r .= $this->encode6bit($c4 & 0x3F);
+         return $r;
+    }
+
+    function encode64($c) {
+         $str = "";
+         $len = strlen($c);
+         for ($i = 0; $i < $len; $i+=3) {
+                if ($i+2==$len) {
+                      $str .= $this->append3bytes(ord(substr($c, $i, 1)), ord(substr($c, $i+1, 1)), 0);
+                } else if ($i+1==$len) {
+                      $str .= $this->append3bytes(ord(substr($c, $i, 1)), 0, 0);
+                } else {
+                      $str .= $this->append3bytes(ord(substr($c, $i, 1)), ord(substr($c, $i+1, 1)), ord(substr($c, $i+2, 1)));
+                }
+         }
+         return $str;
     }
 
     /**
